793d2534d0ae5241eff319f3a15b8e2a531e9ee8
Tom Schuster <tschuster@mozilla.com>
2026-01-28 11:33:37 +0100

    remove todo

--- dom/script/ScriptLoadHandler.cpp
+++ dom/script/ScriptLoadHandler.cpp
@@ -438,8 +438,6 @@ ScriptLoadHandler::OnStreamComplete(nsIIncrementalStreamLoader* aLoader,
     doc = node->OwnerDoc();
   }
 
-  // TODO!!!: Need to duplicate aData!
-
   if (doc) {
     if (auto* integrity = IntegrityPolicy::Cast(
             PolicyContainer::GetIntegrityPolicy(doc->GetPolicyContainer()))) {
