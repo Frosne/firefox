bf311296d98dfc535a053b670002ed08c3070566
Tom Schuster <tschuster@mozilla.com>
2026-02-16 14:08:59 +0100

    Implement WAICT core functionality

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -1602,6 +1602,7 @@ class Document : public nsINode,
   nsresult InitPolicyContainer(nsIChannel* aChannel);
   nsresult InitCSP(nsIChannel* aChannel);
   nsresult InitIntegrityPolicy(nsIChannel* aChannel);
+  nsresult InitIntegrityPolicyWAICT(nsIChannel* aChannel);
   nsresult InitCOEP(nsIChannel* aChannel);
   nsresult InitDocPolicy(nsIChannel* aChannel);
   nsresult InitTLSCertificateBinding(nsIChannel* aChannel);
